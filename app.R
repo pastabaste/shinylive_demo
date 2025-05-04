f842a74e0732cc75cee56a00ad463fc0542e1821
lukas.bruewer <lukas.bruewer@stud.uni-goettingen.de>
2025-05-04 16:09:18 +0200

    shinylive test

--- app.R
+++ app.R
@@ -18,7 +18,7 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",±±±
+            sliderInput("bins",
                         "Number of bins:",
                         min = 1,
                         max = 50,
